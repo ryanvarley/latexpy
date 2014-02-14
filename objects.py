4e1e1ef4f2faf7c328117d5ce4cf3eae5d368eb8
Ryan Varley <ryanjvarley@gmail.com>
2014-02-14 17:02:19 +0000

    initial commit

---
+++ objects.py
@@ -0,0 +1,29 @@
+class LatexObject(object):
+
+
+class LatexFigure(object):
+    """ Handles a figure instance
+    """
+
+    def __init__(self, rows, colums):
+
+        # TODO generate start of figure
+
+class LatexTable(object):
+    """ Handles a table instance
+    """
+
+    def __init__(self, rows, colums):
+
+        # TODO generate start of table
+
+
+    def endTable(self):
+
+        # appends the end of table
+        pass
+
+    def getTEX(self):
+
+        # turn results into a TEX string
+        pass
