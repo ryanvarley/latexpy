7517baa292ea5af8fd4f1569e6973855da650c6b
Ryan Varley <ryanjvarley@gmail.com>
2014-02-14 18:14:07 +0000

    changed format of multi answer test to accommodate test variations

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -5,25 +5,25 @@ import latexpy.objects as tex
 
 class Test_MultiFigure(unittest.TestCase):
 
+    def setUp(self):
+        self.answerStart = ['\\begin{figure}[htbp]', '\\centering', '\\begin{tabular}{cc}']
+        self.answerContent = ['\\includegraphics[width=0.5\\textwidth]{path1.png}&',
+                      '\\includegraphics[width=0.5\\textwidth]{path2.png}\\\\',
+                      '\\includegraphics[width=0.5\\textwidth]{path3.png}&',
+                      '\\includegraphics[width=0.5\\textwidth]{path4.png}\\\\']
+        self.answerEnd = ['\\end{tabular}', '\\end{figure}']
+
+        self.basicFig = tex.LatexMultiFigure()
+        self.basicFig.addFigure('path1.png')
+        self.basicFig.addFigure('path2.png')
+        self.basicFig.addFigure('path3.png')
+        self.basicFig.addFigure('path4.png')
+
     def test_MultiFigureParse2cols(self):
 
-        fig = tex.LatexMultiFigure()
-        fig.addFigure('path1.png')
-        fig.addFigure('path2.png')
-        fig.addFigure('path3.png')
-        fig.addFigure('path4.png')
+        answer = '\n'.join(self.answerStart + self.answerContent + self.answerEnd)
 
-        answer = '\\begin{figure}[htbp]\n' \
-                      '\\centering\n' \
-                      '\\begin{tabular}{cc}\n' \
-                      '\\includegraphics[width=0.5\\textwidth]{path1.png}&\n' \
-                      '\\includegraphics[width=0.5\\textwidth]{path2.png}\\\\\n' \
-                      '\\includegraphics[width=0.5\\textwidth]{path3.png}&\n' \
-                      '\\includegraphics[width=0.5\\textwidth]{path4.png}\\\\\n' \
-                      '\\end{tabular}\n' \
-                      '\\end{figure}'
-
-        self.assertEqual(answer, fig.output())
+        self.assertEqual(answer, self.basicFig.output())
 
 if __name__ == '__main__':
     unittest.main()
